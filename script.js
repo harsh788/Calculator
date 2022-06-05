1b1dee2762596c2ca979a52d6e6e65d0466a632f
harsh788 <Harsh.Shah@iiitb.org>
2022-06-05 19:43:21 +0530

    All buttons work correctly. Result displayed on screen

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let display = 0;
+let num1 = 0;
+let operator = 0;
+let a = 0;
+
 function add(a, b){
     return a+b;
 }
@@ -40,3 +45,66 @@ function operate(operator, a, b){
         return "ERROR";
     }
 }
+
+let input = document.querySelectorAll("button.number");
+let arrInput = Array.from(input);
+arrInput.forEach((div, index) => div.addEventListener("click", ()=> {
+    num1 += arrInput[index].id;
+}));
+
+let division = document.querySelector("#divide");
+let multiplication = document.querySelector("#multiply");
+let addition = document.querySelector("#add");
+let subtraction = document.querySelector("#subtract");
+let equals = document.querySelector("#equals");
+let clear = document.querySelector("#clear");
+let percent = document.querySelector("#percent");
+let plusminus = document.querySelector("#plusminus");
+let dot = document.querySelector("dot");
+let displaybutton = document.querySelector("#display");
+
+division.addEventListener("click", () => {
+    operator = "/";
+    a = num1;
+    num1 = "";
+})
+multiplication.addEventListener("click", () => {
+    operator = "*";
+    a = num1;
+    num1 = "";
+})
+addition.addEventListener("click", () => {
+    operator = "+";
+    a = num1;
+    num1 = "";
+})
+subtraction.addEventListener("click", () => {
+    operator = "-";
+    a = num1;
+    num1 = "";
+})
+equals.addEventListener("click", () => {
+    display = operate(operator, Number(a), Number(num1));
+    a = "";
+    num1 = display;
+    displaybutton.innerHTML = display;  
+})
+clear.addEventListener("click", () => {
+    display = 0;
+    displaybutton.innerHTML = display;
+    a = "";
+    num1 = "";
+})
+percent.addEventListener("click", () => {
+    display = operate("/", Number(display), 100);
+    num1 = display;
+    displaybutton.innerHTML = display;
+})
+plusminus.addEventListener("click", () => {
+    display = operate("*", Number(display), -1);
+    num1 = display;
+    displaybutton.innerHTML = display;
+})
+dot.addEventListener("click", () => {
+    num1 += ".";
+})
