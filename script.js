e3b5d8d9425833c274bd2683825bc54191cc48a2
harsh788 <Harsh.Shah@iiitb.org>
2022-06-05 15:40:49 +0530

    Basic functions and HTML done

---
+++ script.js
@@ -0,0 +1,42 @@
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    if(b!=0)
+    {
+        return a/b;
+    }
+    else
+    {
+        return "ERROR";
+    }
+}
+
+function operate(operator, a, b){
+    if(operator === "+")
+    {
+        return add(a, b);
+    }
+    else if(operator === "-")
+    {
+        return subtract(a, b);
+    }
+    else if(operator === "*")
+    {
+        return multiply(a, b);
+    }
+    else if(operator === "/")
+    {
+        return divide(a, b);
+    }
+    else
+    {
+        return "ERROR";
+    }
+}
